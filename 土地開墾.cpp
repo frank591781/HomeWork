0136b71715ecb05dfba768958ac586bf8bbc9afd
陳彥均 <91167242+frank591781@users.noreply.github.com>
2021-12-24 11:00:13 +0800

    homework week 5

--- 土地開墾.cpp
+++ 土地開墾.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int l, w;
-    //input
+        //input
     while (cin >> l >> w)
     {
         if (l == 0 && w == 0)
@@ -24,7 +24,7 @@ int main()
                 }
             }
         }
-        //sort
+            //sort
         for (int i = 0; i < l; i++)
         {
             for (int j = 0; j < w; j++)
@@ -39,29 +39,50 @@ int main()
                 }
             }
         }
-        //sort
+            //sort
         for (int i = 1; i < l; i++)
         {
             for (int j = 1; j < w; j++)
-                ans[i][j] = min(ans[i - 1][j], ans[i][j - 1], ans[i - 1][j - 1]) + 1;
+                if(ans[i][j]==0)
+                {
+                    continue;
+                }
+                else
+                    ans[i][j] = min(ans[i-1][j-1],min(ans[i-1][j],ans[i][j-1]))+1;
         }
-        // testout
-        for (int i = 0; i < l; i++)
+            // testout
+        // for (int i = 0; i < l; i++)
+        // {
+        //     for (int j = 0; j < w; j++)
+        //     {
+        //         cout << set[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // for (int i = 0; i < l; i++)
+        // {
+        //     for (int j = 0; j < w; j++)
+        //     {
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+            //find the largest length
+        int max_length,tmp;
+        max_length=0;
+        for(int i=0;i<l;i++)
         {
-            for (int j = 0; j < w; j++)
+            for(int j=0;j<w;j++)
             {
-                cout << set[i][j] << " ";
+                tmp=ans[i][j];
+                if(tmp>max_length)
+                    swap(max_length,tmp);
             }
-            cout << endl;
         }
-        for (int i = 0; i < l; i++)
-        {
-            for (int j = 0; j < w; j++)
-            {
-                cout << ans[i][j] << " ";
-            }
-            cout << endl;
-        }
+        cout << max_length*max_length << endl;
     }
     return 0;
 }
+
+//https://e-tutor.itsa.org.tw/e-Tutor/file.php/2627/HW5_1.jpg
+//https://e-tutor.itsa.org.tw/e-Tutor/file.php/2627/HW5_2.jpg
